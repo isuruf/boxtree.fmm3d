5ebf4ef667226863ba8c6cc472d71c3e9aa3e771
Isuru Fernando <idf2@illinois.edu>
2022-02-05 16:55:10 -0600

    add an expansion order finder

---
+++ boxtree/fmm3d/level_to_order.py
@@ -0,0 +1,27 @@
+from boxtree.fmm3d.fortran import l3dterms, h3dterms
+from sumpy.kernel import LaplaceKernel, HelmholtzKernel
+import numpy as np
+
+
+class FMM3DExpansionOrderFinder:
+
+    def __init__(self, eps, extra_order=0):
+        self.eps = eps
+        self.extra_order = extra_order
+
+    def __call__(self, kernel, kernel_args, tree, level):
+
+        nterms = np.empty(1, dtype=np.int32)
+
+        if isinstance(kernel, LaplaceKernel):
+            # src/Laplace/lfmm3d.f#L392
+            l3dterms(self.eps, nterms[0:1])
+
+        elif isinstance(kernel, HelmholtzKernel):
+            b0 = tree.root_extent
+            zk = kernel_args[kernel.helmholtz_k_name]
+            zkfmm = np.complex128(zk * b0)
+            boxsize = b0 / 2 ** level
+            h3dterms(boxsize, zkfmm, self.eps, nterms[0:1])
+
+        return nterms[0] + self.extra_order
